b3f99800d32141cbd4e0b7a7d9fac5934c27e8a5
Caio <caiorodriguesmll@gmail.com>
2023-07-26 18:51:34 -0400

    Adding Template

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -3,6 +3,6 @@ import React from "react";
 export default function ItemComponent(props){
     const status = props.status;
     return  <li>{ props.name } 
-            <div>Status: {status ? <div>Finalizado</div> : <div>Não Finalizado</div>}</div>
+            <div>Status da compra: {status ? <div>Finalizado!</div> : <div>Não Finalizado!</div>}</div>
             </li>
 }
