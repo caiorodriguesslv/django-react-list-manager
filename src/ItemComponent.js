5f23b318fc9a9f2b1c1d2d7c5bc0020f92a49df2
Caio <caiorodriguesmll@gmail.com>
2023-07-26 02:21:31 -0400

    Listing items of each list

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function ItemComponent(props){
-    return <li>Item Description:{ props.name }</li>
+    const status = props.status;
+    return  <li>{ props.name } 
+            Status: {status ? <div>Finalizado</div> : <div>Não Finalizado</div>}
+            </li>
 }
