ce7f12d734e557f8da023ee415e1e938f24ead00
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-08-23 19:06:43 -0300

    Fixing bug

--- src/LoginComponent.js
+++ src/LoginComponent.js
@@ -1,66 +1,75 @@
 import React from "react";
 import UserLists from "./UserLists";
 
-export default class LoginComponent extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {username: '', password: '', lists: []};
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-      }
-    
-      handleChange(event) {
-        this.setState({username: event.target.value});
-      }
+export default class LoginComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password: '', lists: [], error: '' };
 
-      handleChangePassword(event) {
-        this.setState({password: event.target.value});
-      }
-    
-      handleSubmit(event) {
-        var url = 'http://127.0.0.1:8000/api-token-auth/';
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: this.state.username, password: this.state.password })
-        };
-        fetch(url, requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                localStorage.setItem('token', data.token);
-                this.setState({token: data.token});
-            });
-        event.preventDefault();
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
 
-      logout(){
-            localStorage.removeItem('token');
-            this.setState({token: null});
-      }
-    
-      render() {
-        var token = localStorage.getItem('token');
+  handleChange(event) {
+    this.setState({ username: event.target.value });
+  }
 
-        if(!token)
-            return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                Digite seu usuário:
-                <input type="text" value={this.state.username} onChange={this.handleChange} />
-                Digite sua senha:
-                <input type="password" value={this.state.password} onChange={this.handleChangePassword} />
-                </label>
-                <input type="submit" value="Logar" />
-            </form>
-            );
-        else
-            return (
-                <div>
-                    <UserLists/>
-                    <button onClick={()=> this.logout()}>Logout</button>
-                </div>
-                )
-      }
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit(event) {
+    var url = 'http://127.0.0.1:8000/api-token-auth/';
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username: this.state.username, password: this.state.password })
+    };
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            this.setState({ token: data.token, error: '' })
+        } else {  
+            this.setState({ error: 'Nome de usuário ou senha incorreta!'})
+        }
+      })
+      .catch(error =>{
+        console.error('Error:', error);
+        this.setState({ error: 'Ocorreu um erro durante a autenticação'});
+      })
+    event.preventDefault();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.setState({ token: null });
+  }
+
+  render() {
+    var token = localStorage.getItem('token');
+
+    if (!token)
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Digite seu usuário:
+            <input type="text" value={this.state.username} onChange={this.handleChange} />
+            Digite sua senha:
+            <input type="password" value={this.state.password} onChange={this.handleChangePassword} />
+          </label>
+          <input type="submit" value="Logar" />
+          <p style={{ color: 'red' }} >{this.state.error}</p> 
+        </form>
+      );
+    else
+      return (
+        <div>
+          <UserLists />
+          <button onClick={() => this.logout()}>Logout</button>
+        </div>
+      )
+  }
 }
