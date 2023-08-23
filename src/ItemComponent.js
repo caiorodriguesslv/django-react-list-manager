b7d53d38e2a865e73e7becf75ed283a3f9c46201
Caio Rodrigues <caiorodriguesmll@gmail.com>
2023-08-23 16:50:46 -0300

    Adding new css

--- src/ItemComponent.js
+++ src/ItemComponent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ItemComponent(props){
     const status = props.status;
-    return  <li>{ props.name } 
+    return  <li>{ "Item: " + props.name } 
             <div>Status da compra: {status ? <div>Finalizado!</div> : <div>Não Finalizado!</div>}</div>
             </li>
 }
